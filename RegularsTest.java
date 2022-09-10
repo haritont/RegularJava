4c2d6fbbaf55b51ea6b957e8ce85ead2f67b3ce4
Vika <haritont180402@gmail.com>
2022-09-10 11:10:00 +0300

    Написала тесты для второго задания

--- RegularsTest.java
+++ RegularsTest.java
@@ -34,4 +34,22 @@ public class RegularsTest {
         Assert.assertEquals(false, regular.checkIP("-8.o.0.8"));
         Assert.assertEquals(false, regular.checkIP("iu,ghj.0.0"));
     }
+    @Test
+    public void checkTrueGUID(){
+        Assert.assertEquals(true, regular.checkGUID("e02fd0e4-00fd-090A-ca30-0d00a0038ba0"));
+        Assert.assertEquals(true, regular.checkGUID("c1af4cf5-674d-4e7a-adbe-401e0163cff5"));
+        Assert.assertEquals(true, regular.checkGUID("1f9959f1-0094-4cfa-a229-27b6f953c4fe"));
+        Assert.assertEquals(true, regular.checkGUID("3148898f-0fea-403e-a106-769f23ab6266"));
+        Assert.assertEquals(true, regular.checkGUID("ffecf9c3-44cb-4a44-b2f8-9b23f0c75d2e"));
+        Assert.assertEquals(true, regular.checkGUID("85a2ccdc-1101-4c4d-8644-77f9518ba2e1"));
+    }
+    @Test
+    public void checkFalseGUID(){
+        Assert.assertEquals(false, regular.checkGUID("e02fd0e400fd090Aca300d00a0038ba0"));
+        Assert.assertEquals(false, regular.checkGUID("c1af4cf5-674d-4e7a-adbe-adff-401e0163cff5"));
+        Assert.assertEquals(false, regular.checkGUID("1f9959f1_0094_4cfa_a229_27b6f953c4fe"));
+        Assert.assertEquals(false, regular.checkGUID("3148898f-0fea-403e-a106769f23ab6266"));
+        Assert.assertEquals(false, regular.checkGUID("fFecf9c3,44CB,4a44,b2f8,9b23F0c75d2e"));
+        Assert.assertEquals(false, regular.checkGUID("85a2ccdc-1101-4c4d-77f9518ba2e1"));
+    }
 }
